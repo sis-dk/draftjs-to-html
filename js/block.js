96b1049602b617cdf94a8f0a3284d206a4033f06
Sisir <sisirvevo@gmail.com>
2018-07-07 02:04:01 +0530

    image alignment issue fix

--- js/block.js
+++ js/block.js
@@ -333,7 +333,10 @@ function getEntityMarkup(
     return `<a href="${entity.data.url}" target="${targetOption}">${text}</a>`;
   }
   if (entity.type === 'IMAGE') {
-    return `<img src="${entity.data.src}" alt="${entity.data.alt}" style="float:${entity.data.alignment || 'none'};height: ${entity.data.height};width: ${entity.data.width}"/>`;
+    let imgAlignment = entity.data.alignment || 'center';
+    let imgHeight = entity.data.height == 'auto' ? '100%' : entity.data.height;
+    let imgWidth = entity.data.width == 'auto' ? '100%' : entity.data.width;
+    return `<div style="width:100%;text-align:${imgAlignment}"><img src="${entity.data.src}" alt="${entity.data.alt}" style="height: ${imgHeight};width: ${imgHeight}"/></div>`;
   }
   if (entity.type === 'EMBEDDED_LINK') {
     return `<iframe width="${entity.data.width}" height="${entity.data.height}" src="${entity.data.src}" frameBorder="0"></iframe>`;
